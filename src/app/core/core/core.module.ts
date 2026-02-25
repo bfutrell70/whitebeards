47ab9976a7028677efd8c1fe9c4982456b238b78
Brian Futrell <bfutrel@gmail.com>
2026-02-25 07:57:56 -0500

    module 4 - The Core Module (To Be or Not to Be)

---
+++ src/app/core/core/core.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { NavBarComponent } from './nav-bar.component';
+import { AccountMenuComponent } from './account-menu.component';
+import { UserRepositoryService } from 'src/app/services/user-repository.service';
+
+@NgModule({
+  declarations: [
+    NavBarComponent, 
+    AccountMenuComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    NavBarComponent, 
+    AccountMenuComponent
+  ],
+  providers: [
+    UserRepositoryService,
+  ]
+})
+export class CoreModule { }
